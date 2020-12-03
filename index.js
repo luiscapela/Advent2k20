73edfe527275cb85d240557e80559e52c5c19455
luiscapela <luis13capela@gmail.com>
2020-12-03 20:28:26 +0000

    add solution for day1

---
+++ index.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+ 
+var contents = fs.readFileSync('input.txt', 'utf8');
+const lines = contents.split("\r\n"); 
+const numbers = lines.map(number=> {
+    return Number(number)
+}); 
+ 
+
+numbers.forEach((number, index) => {
+  
+    const followingNumbers = numbers.slice(index + 1) 
+    followingNumbers.forEach(nextNumber => { 
+        const sum = number + nextNumber 
+        if (sum === 2020) { 
+            console.log("Apanhei-te",number, nextNumber) 
+            console.log(number * nextNumber)
+            
+        }
+            
+    });
+});  
+
