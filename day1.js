fbe0afb939f51ecfae25be71a1ed1d2ad66eb421
Rui Fonseca <rui.fonseca@thetrainline.com>
2020-12-04 18:36:53 +0000

    Add day1 second part solution

--- day1.js
+++ day1.js
@@ -1,7 +1,7 @@
 var fs = require("fs");
 
 var contents = fs.readFileSync("day1input.txt", "utf8");
-const lines = contents.split("\r\n");
+const lines = contents.split(/\r?\n/);
 const numbers = lines.map((number) => {
   return Number(number);
 });
@@ -14,5 +14,19 @@ numbers.forEach((number, index) => {
       console.log("Apanhei-te", number, nextNumber);
       console.log(number * nextNumber);
     }
+  });
+});
+
+numbers.forEach((number, index) => {
+  const followingNumbers = numbers.slice(index + 1);
+  followingNumbers.forEach((nextNumber, nextNumberIndex) => {
+    const nextNumberFollowingNumbers = numbers.slice(nextNumberIndex + 1);
+    nextNumberFollowingNumbers.forEach((nextNextNumber) => {
+      const sum = number + nextNumber + nextNextNumber;
+      if (sum === 2020) {
+        console.log("Apanhei-te outra vez", number, nextNumber, nextNextNumber);
+        console.log(number * nextNumber * nextNextNumber);
+      }
+    });
   });
 });
