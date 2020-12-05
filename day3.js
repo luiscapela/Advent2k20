aaf1faf1aff27db7e539bd0204b8900da67cdfd0
luiscapela <luis13capela@gmail.com>
2020-12-05 16:36:23 +0000

    Answer the adventofcode day3 part2

--- day3.js
+++ day3.js
@@ -1,29 +1,41 @@
 const fs = require("fs");
 
 const contents = fs.readFileSync("day3input.txt", "utf8");
-const lines = contents.split(/\r?\n/); 
-const firstLine = lines[0]; 
-const patternLength = firstLine.length  
-let numberOfTrees = 0
+const lines = contents.split(/\r?\n/);
+const firstLine = lines[0];
+const patternLength = firstLine.length;
+let numberOfTrees = 0;
 
-
- 
-lines.forEach((line, index)=>{
-    if (index === 0 ) {
-        return 
-    } 
-    const position = (index*3) % patternLength;  
-     
-    const place = line[position] 
-     
-    if (place === "#") {
-       numberOfTrees = numberOfTrees + 1   
-       
+const calculateNumberOfTrees = (rightNumber, downNumber) => {
+  let numberOfTrees = 0;
+  const availableLines = lines.filter((line, index) => {
+    if (index % downNumber === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  availableLines.forEach((line, index) => {
+    if (index === 0) {
+      return;
     }
 
+    const position = (index * rightNumber) % patternLength;
 
+    const place = line[position];
 
+    if (place === "#") {
+      numberOfTrees = numberOfTrees + 1;
+    }
+  });
+  return numberOfTrees;
+};
 
-
-})  
-console.log("Number: ",numberOfTrees)
+const totalNumberOfTrees =
+  calculateNumberOfTrees(1, 1) *
+  calculateNumberOfTrees(3, 1) *
+  calculateNumberOfTrees(5, 1) *
+  calculateNumberOfTrees(7, 1) *
+  calculateNumberOfTrees(1, 2);
+console.log("Number part2: ", totalNumberOfTrees);
+console.log("Number part1: ", calculateNumberOfTrees(3, 1));
