45e1a9328a597761540a6136d0a7767d1368ff68
luiscapela <luis13capela@gmail.com>
2020-12-05 19:14:09 +0000

    Day4 the advent of code 2020 solutions part2

--- day4.js
+++ day4.js
@@ -25,4 +25,87 @@ lines.forEach((line) => {
   }
 });
 
-console.log("Number of valid passports", numberOfValidPassaport);
+console.log("Number of valid passports PART1  ", numberOfValidPassaport);
+//PART1
+
+numberOfValidPassaport = 0;
+lines.forEach((line) => {
+  const passportData = line.split(/ |\r?\n/);
+  const passportKeys = passportData.map((passportEntry) => {
+    const passportValues = passportEntry.split(/:/);
+    const passportKey = passportValues[0];
+    const passportValue = passportValues[1];
+    if (passportKey === "byr") {
+      const numberPassportValue = new Number(passportValue);
+      if (numberPassportValue < 1920 || numberPassportValue >= 2003) {
+        return "";
+      }
+    }
+    if (passportKey === "iyr") {
+      const numberPassportValue = new Number(passportValue);
+      if (numberPassportValue < 2010 || numberPassportValue > 2020) {
+        return "";
+      }
+    }
+    if (passportKey === "eyr") {
+      const numberPassportValue = new Number(passportValue);
+      if (numberPassportValue < 2020 || numberPassportValue > 2030) {
+        return "";
+      }
+    }
+
+    if (passportKey === "hgt") {
+      const measure = passportValue.slice(-2);
+      if (measure === "cm") {
+        const height = new Number(passportValue.slice(0, -2));
+        if (height < 150 || height > 193) {
+          return "";
+        }
+      } else if (measure === "in") {
+        const height = new Number(passportValue.slice(0, -2));
+        if (height < 59 || height > 76) {
+          return "";
+        }
+      } else {
+        return "";
+      }
+    }
+    if (passportKey === "hcl") {
+      if (passportValue.match(/#([0-9]|[a-f]){6}/) === null) {
+        return "";
+      }
+    }
+    if (passportKey === "ecl") {
+      if (
+        passportValue !== "amb" &&
+        passportValue !== "blu" &&
+        passportValue !== "brn" &&
+        passportValue !== "gry" &&
+        passportValue !== "grn" &&
+        passportValue !== "hzl" &&
+        passportValue !== "oth"
+      ) {
+        return "0";
+      }
+    }
+    if (passportKey === "pid") {
+      if (passportValue.match(/^[0-9]{9}$/) === null) {
+        return "";
+      }
+    }
+    return passportValues[0];
+  });
+
+  if (
+    passportKeys.includes("byr") &&
+    passportKeys.includes("iyr") &&
+    passportKeys.includes("eyr") &&
+    passportKeys.includes("hgt") &&
+    passportKeys.includes("hcl") &&
+    passportKeys.includes("ecl") &&
+    passportKeys.includes("pid")
+  ) {
+    numberOfValidPassaport = numberOfValidPassaport + 1;
+  }
+});
+console.log("Number of valid passports PART2", numberOfValidPassaport);
