1bf6a5ab3f89855007b84e6c6d9574eb841cddf8
luiscapela <luis13capela@gmail.com>
2020-12-10 20:20:23 +0000

    solution of day6 part2 advent of the code 2k20

--- day6.js
+++ day6.js
@@ -1,8 +1,10 @@
+const { group } = require("console");
 const fs = require("fs");
 
 const contents = fs.readFileSync("day6input.txt", "utf8");
 const groups = contents.split(/\r?\n\r?\n/);
 let sumOfQuestions = 0;
+let numberOfQuestions = 0;
 groups.forEach((group) => {
   const yesQuestions = new Set();
 
@@ -17,3 +19,23 @@ groups.forEach((group) => {
   sumOfQuestions = yesQuestions.size + sumOfQuestions;
 });
 console.log("Number of yes questions: ", sumOfQuestions);
+
+groups.forEach((group) => {
+  const lines = group.split(/\r?\n/);
+  const firstLine = lines[0];
+  const lettersFirstLine = firstLine.split("");
+  lettersFirstLine.forEach((letterFirstLine) => {
+    const letterExistInEveryLine = lines.slice(1).every((line) => {
+      if (line.includes(letterFirstLine)) {
+        return true;
+      }
+
+      return false;
+    });
+
+    if (letterExistInEveryLine) {
+      numberOfQuestions = numberOfQuestions + 1;
+    }
+  });
+});
+console.log("Every yes questions: ", numberOfQuestions);
